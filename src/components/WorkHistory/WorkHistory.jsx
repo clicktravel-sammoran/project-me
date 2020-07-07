f96af571b5359d98fb5ce7289dd2a3c86eee0872
Sam <sam.moran@clicktravel.com>
2020-07-07 15:00:32 +0100

    add url for companies worked at

--- src/components/WorkHistory/WorkHistory.jsx
+++ src/components/WorkHistory/WorkHistory.jsx
@@ -2,19 +2,19 @@ import React from 'react'
 import { Card, Image, Container } from 'react-bootstrap'
 import workHistory from '../../data/work_history'
 import styles from './WorkHistory.module.css'
-import Pills from '../Pills/Pills.jsx'
+import Pills from '../Pills/Pills'
 
 const WorkHistory = () => {
     return (
         <div>
             {workHistory.map(job => {
-                const { current, jobTitle, company, logo, date, description, keyPoints, techStack } = job
+                const { current, jobTitle, company, logo, date, description, keyPoints, techStack, link } = job
                 return (
                     <Card border={current ? 'warning' : 'clear'} className={styles.Card} key={`${company}Card`}>
                         <Container className={styles.Container}>
                             <Image src={logo} rounded className={styles.Logo} />
                             < Card.Body >
-                                <Card.Title>{jobTitle} at {company}</Card.Title>
+                                <Card.Title>{jobTitle} @ <a href={`https://${link}`}>{company}</a></Card.Title>
                                 <Card.Subtitle>{date}</Card.Subtitle>
                                 <Card.Text>{description}</Card.Text>
                                 <Container className={styles.PillContainer}>
