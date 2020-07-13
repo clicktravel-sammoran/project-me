7af6b3cd21017073dd1880e99b24acbc3942a736
Sam <sam.moran@clicktravel.com>
2020-07-13 16:35:05 +0100

    Data for projects page

---
+++ src/data/projects.js
@@ -0,0 +1,26 @@
+const projects = [{
+    title: "Portfolio Site",
+    description: "This very site!",
+    url: "https://github.com/clicktravel-sammoran/project-me",
+    techStack: ["React", "Bootstrap", "CSS Modules", "nodejs"],
+},
+{
+    title: "Toucan Doodle",
+    description: "Native Android App built in Kotlin utilising Google Cloud where users can register and then log into a whiteboard for real time, collaborative drawing. ",
+    url: "https://www.youtube.com/watch?v=VM9JgyZbN0k",
+    techStack: ["Kotlin", "Firebase authentification", "Realtime database", "Cloud storage"]
+},
+{
+    title: "NC-News api",
+    description: "A api connected to a PostgreSQL database, serving up articles and their corresponding comments and allowing new ones to be created",
+    url: "https://github.com/angriestofhippos/nc-news",
+    techStack: ["express", "Knex", "PostgreSQL", "Mocha", "Chai", "Supertest", "Heroku"]
+},
+{
+    title: "Northcoders News",
+    description: "Reddit style news site, populating with articles that could be voted and commented",
+    url: "https://github.com/angriestofhippos/Northcoders-News",
+    techStack: ["React", "CSS Modules", "Reach Router", "Axios"]
+}]
+
+export default projects
